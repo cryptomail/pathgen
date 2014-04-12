b913f667d41363e1469e6f9d8f4a3214b1cf92d7
Joshua Teitelbaum <joshuat@gmail.com>
2014-04-11 17:02:24 -0700

    checkpoint

--- js/animationbuilder.js
+++ js/animationbuilder.js
@@ -2,36 +2,159 @@
  * Created by joshuateitelbaum on 4/5/14.
  */
 "use strict";
-function Animation(n,w,h)
+function KeyFrameBlock(par,name)
 {
-	
+    this.name = name;
+    this.par = par;
+    this.editimagesrc= ko.observable("");
+    this.imageSrcs = ko.observableArray([]);
+
+    this.addImageSource = function()
+    {
+        if(this.editimagesrc == null || this.editimagesrc() == null || this.editimagesrc().length <= 0)
+        {
+
+            return;
+        }
+
+        this.imageSrcs.push(this.editimagesrc());
+
+    }
+    this.removeImageSource = function()
+    {
+        var idx = this.imageSrcs().indexOf(this.editimagesrc());
+        if(idx < 0)
+        {
+            animationbuilder._emitError(3);
+            return;
+        }
+
+        if (idx > -1) {
+            this.imageSrcs.splice(idx, 1);
+        }
+        if(this.imageSrcs().length > 0)
+        {
+            this.par.par.selectedimagesrc(this.imageSrcs()[0]);
+        }
+    }
+}
+function Animation(p,n,w,h)
+{
+	this.par = p;
 	this.name =n;
-	this.width = w;
-	this.height = h;
+	this.width = ko.observable(w);
+	this.height = ko.observable(h);
+    this.keyFrameBlocks= ko.observableArray([]);
+    this.editkeyframeblock = ko.observable(new KeyFrameBlock(this,""));
+
+
+    this.addImageSource = function()
+    {
+        this.editkeyframeblock().addImageSource();
+
+    },
+    this.removeImageSource = function()
+    {
+            this.editkeyframeblock().removeImageSource();
+
+    },
+    this.optionsImageSourceChanged = function(newvalue)
+    {
+        this.editkeyframeblock().editimagesrc(newvalue);
+    },
+    this.optionsKeyBlockChanged =  function(newvalue)
+    {
+        if(!newvalue)
+            return;
+
+        var block = this.findKeyFrameBlock(newvalue.name);
+
+
+        if(block)
+        {
+            var newObject = jQuery.extend(true, {}, newvalue);
+            this.editkeyframeblock(newObject);
+
+        }
+    }
+    this.findKeyFrameBlock = function(name)
+    {
+        var x;
+        var n;
+        n = this.keyFrameBlocks().length;
+        for(x=0; x < n; x++)
+        {
+            if(this.keyFrameBlocks()[x].name == name)
+            {
+                return this.keyFrameBlocks()[x];
+            }
+        }
+
+        return null;
+    }
+    this.addKeyFrameBlock = function()
+    {
+        var self = this;
+
+
+        if(self.editkeyframeblock().name.length <= 0)
+        {
+            animationbuilder._emitError(3);
+            return;
+        }
+        var block = self.findKeyFrameBlock(self.editkeyframeblock().name);
+        if(block != null)
+        {
+            animationbuilder._emitError(4);
+            return;
+        }
+        var block = new KeyFrameBlock(self,self.editkeyframeblock().name);
+        self.keyFrameBlocks.push(block);
+        self.par.selectedkeyframeblock(block);
+
+
+    }
+    this.removeKeyFrameBlock = function()
+    {
+        var block = this.findKeyFrameBlock(this.editkeyframeblock().name);
+        if(block == null)
+        {
+            animationbuilder._emitError(3);
+            return;
+        }
+        var idx = this.keyFrameBlocks.indexOf(block);
+        if (idx > -1) {
+            this.keyFrameBlocks.splice(idx, 1);
+        }
+        if(this.keyFrameBlocks().length > 0)
+        {
+            var newObject = jQuery.extend(true, {}, this.keyFrameBlocks()[0]);
+            this.par.selectedkeyframeblock(newObject);
+        }
+    }
+
 }
 
 var animationbuilder = {
 	MAX_ANIMATION_WIDTH:320,
 	MAX_ANIMATION_HEIGHT:240,
-	animationnames:ko.observableArray([]),
-	selectedanimationname:ko.observable(""),
-	selectedanimationwidth:ko.observable(0),
-	selectedanimationheight:ko.observable(0),
-	availableanimations:ko.observableArray([]),
-	optionsanimationname:ko.observable(""),
-	selectedkeyframeblockname:ko.observable(""),
-	keyframeblocknames:ko.observableArray([]),
+	animations:ko.observableArray([]),
+    selectedanimation:ko.observable({}),
+    editanimation:ko.observable(new Animation(this,"",0,0)),
+    selectedkeyframeblock:ko.observable({}),
+    selectedimagesrc:ko.observable(""),
 
-	animations:{},
-	
-_emitError: function()
+
+
+    _emitError: function()
     {
         var errormap = {
             1:"Invalid animation width.",
             2:"Invalid animation height",
             3:"Invalid animation name",
             4:"Animation name alread exists",
-            5:"No animations registered and chosen, cannot continue"
+            5:"No animations registered and chosen, cannot continue",
+            6:"Keyframe block name invalid"
         };
 
         var eno = arguments[0];
@@ -56,89 +179,139 @@ _emitError: function()
 		var self = document.animationbuilder;
 		if(ui.oldPanel.attr("id") == "animationnames")
 		{
-			if((!self.selectedanimationname) || (!self.selectedanimationname()))
+
+			if((!self.editanimation) || (!self.editanimation()) || (self.editanimation().name.length <= 0))
 			{
 				self._emitError(5);
 				return false;
 			}
 		}
+        else if(ui.newPanel.attr("id") == "keyframes")
+        {
+
+        }
 	},
 	initialize:function()
 	{
-		this.optionsanimationname.subscribe(this.optionsAnimationNameChanged,this);
+        this.selectedanimation.subscribe(this.optionsAnimationChanged,this);
+        this.selectedkeyframeblock.subscribe(this.optionsKeyBlockChanged,this);
+        this.selectedimagesrc.subscribe(this.optionsImageSourceChanged,this);
 		ko.applyBindings(this);
 		$( "#tabs" ).on( "tabsbeforeactivate", this.beforeActivate );
 	},
-	optionsAnimationNameChanged: function(newvalue)
+    addKeyFrameBlock:function()
+    {
+        this.editanimation().addKeyFrameBlock();
+    },
+    removeKeyFrameBlock:function()
+    {
+        this.editanimation().removeKeyFrameBlock();
+    },
+    optionsImageSourceChanged:function(newvalue)
+    {
+        this.editanimation().optionsImageSourceChanged(newvalue);
+    },
+    optionsKeyBlockChanged: function(newvalue)
+    {
+        this.editanimation().optionsKeyBlockChanged(newvalue);
+    },
+	optionsAnimationChanged: function(newvalue)
 	{
-		this.selectedanimationname(newvalue);
-		var item = this.animations[newvalue];
-		if(item)
-		{
-			this.selectedanimationwidth(item.width);
-			this.selectedanimationheight(item.height);
-		}
+        if(!newvalue)
+        return;
+
+        var animation = this.findAnimationByName(newvalue.name);
+
+
+        if(animation)
+        {
+
+            var newObject = jQuery.extend(true, {}, newvalue);
+            this.editanimation(newObject);
+        }
 	},
 	removeAnimationName: function()
 	{
-		var self = this;
-		if(!self.selectedanimationname())
-		{
-			return;
-		}
-		if(self.selectedanimationname().length <= 0)
-		{
-			return;
-		}
-		if(self.animations[self.selectedanimationname()])
-		{
-			var  item = self.animations[self.selectedanimationname()];
-			self.animationnames.remove(item.name);
-			self.availableanimations.remove(item.name);
-			if(item)
-			{
-				delete self.animations[item.name];
-			}
-		}
-		if(self.animationnames().length > 0)
-		{
-			var item = self.animationnames()[0];
-			self.optionsanimationname(item.name);
-		}
-		
+        var animation = this.findAnimationByName(this.editanimation().name);
+        if(animation == null)
+        {
+            this._emitError(3);
+            return;
+        }
+        var idx = this.animations.indexOf(animation);
+        if (idx > -1) {
+            this.animations.splice(idx, 1);
+        }
+        if(this.animations().length > 0)
+        {
+            var newObject = jQuery.extend(true, {}, this.animations()[0]);
+            this.editanimation(newObject);
+
+        }
 	},
+
+
+    findAnimationByName:function(name)
+    {
+        var animation = null;
+        var x;
+        var n;
+        n = this.animations().length;
+        for(x=0;x < n; x++)
+        {
+            if(this.animations()[x].name == name)
+            {
+                return this.animations()[x];
+            }
+
+        }
+        return null;
+
+    },
 	addAnimationName: function()
 	{
 		var self = this;
 
-		if(self.selectedanimationwidth() <= 0 || self.selectedanimationwidth() > self.MAX_ANIMATION_WIDTH)
-		{
-			self._emitError(1);
-			return;
-		}
-		if(self.selectedanimationheight() <= 0 || self.selectedanimationheight() > self.MAX_ANIMATION_HEIGHT)
-		{
-			self._emitError(2);
-			return;
-		}
 
-		if((!self.selectedanimationname()) || self.selectedanimationname().length <= 0)
+		if(self.editanimation().name.length <= 0)
 		{
 			self._emitError(3);
 			return;
 		}
-		if(self.availableanimations().indexOf(self.selectedanimationname()) != -1)
-		{
-			self._emitError(4);
-			return;
-		}
-		var animation = new Animation(self.selectedanimationname(),self.selectedanimationwidth(),self.selectedanimationheight());
-		self.animations[self.selectedanimationname()] = animation;
-		self.availableanimations.push(self.selectedanimationname());
-		self.optionsanimationname(self.selectedanimationname());
-		self.animationnames.push(self.selectedanimationname());
+        var animation = self.findAnimationByName(self.editanimation().name);
+        if(animation != null)
+        {
+            self._emitError(4);
+            return;
+        }
+		var animation = new Animation(self,self.editanimation().name,self.editanimation().width(),self.editanimation().height());
+		self.animations.push(animation);
+        self.selectedanimation(animation);
 
-	}
+	},
+    addImageSource: function()
+    {
+        var self = this;
+        if(self.editanimation().name.length <= 0)
+        {
+            self._emitError(3);
+            return;
+        }
 
+        self.editanimation().addImageSource();
+
+    },
+    removeImageSource: function()
+    {
+        var self = this;
+        if(self.editanimation().name.length <= 0)
+        {
+            self._emitError(3);
+            return;
+        }
+
+        self.editanimation().removeImageSource();
+
+    }
 
 };
